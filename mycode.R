92dc09fa5897bdd1264e3e9874818fe92fe36596
rcflorestal <rcflorestal@yahoo.com.br>
2020-11-01 14:05:33 -0300

    R console Input and Evaluation

--- mycode.R
+++ mycode.R
@@ -1,3 +1,4 @@
+# Getting Started and R Nuts and Bolts
 myfunction <- function() {
   x <- rnorm(100)
   mean(x)
@@ -9,3 +10,20 @@ second <- function(x) {
 
 second(1)
 second(2)
+
+# R Console Input and Evaluation
+x <- 1
+
+msg <- "Hello"
+# The hash symbol indicates a comment
+
+x ## Auto-printing
+
+print(x) ## Explicit printing
+
+# The operator : is used to create a sequence
+x <- 1:20  ## Creates a sequence of integers from 1 to 20
+
+# When an R vector is printed you will notice that an index for the vector is 
+# printed in square brackets [] on the side.
+x
