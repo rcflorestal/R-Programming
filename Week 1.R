a6b74df00802b1f6f03c9bc289abb73a8424627f
rcflorestal <rcflorestal@yahoo.com.br>
2020-11-02 19:47:05 -0300

    Subsetting lists

--- Week 1.R
+++ Week 1.R
@@ -177,3 +177,28 @@ x[x > "a"]
 u <- x > "a"
 u
 x[u]
+
+
+## 
+x <- list(foo = 1:4, bar = 0.6)
+x
+x[1]
+x[[1]]
+x$bar
+
+x <- list(foo = 1:4, bar = 0.6, baz = "hello")
+x
+x[c(1,3)]
+x$baz
+
+x <- list(foo = 1:4, bar = 0.6, baz = "hello")
+name <- "foo"
+x[[name]]  ## computed index for 'foo'
+x$name     ## element ‘name’ doesn’t exist!
+x$foo      ## element ‘foo’ does exist
+
+# The [[ can take an integer sequence.
+x <- list(a = list(10, 12, 14), b = c(3.14, 2.81))
+x[[c(1, 3)]]
+x[[1]][[3]]
+x[[c(2, 1)]]
