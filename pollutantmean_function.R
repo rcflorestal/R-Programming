ac14fa3f871e2046981391218ca34f3b58b811ca
rcflorestal <rcflorestal@yahoo.com.br>
2020-11-11 19:41:57 -0300

    Update Pollutant function

--- pollutantmean_function.R
+++ pollutantmean_function.R
@@ -3,15 +3,15 @@
 
 pollutantmean <- function(directory, pollutant, id = 1:332){
         ## Create a empty data frame
-        df <- NA
+        df <- data.frame()
         ## Looping to read each file in the directory
         for(i in id){
                 files <- read.csv(paste(directory, 
                                         "/", 
                                         sprintf("%03d", i), 
                                         ".csv", 
-                                        sep ="",
-                                        header = TRUE))
+                                        sep =""
+                                        ))
                 ## Merge all files in a single data frame
                 df <- rbind(df, files)
         }
@@ -25,4 +25,3 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
 pollutantmean("specdata", "sulfate", 1:10)      ## 4.064128
 pollutantmean("specdata", "nitrate", 70:72)     ## 1.706047
 pollutantmean("specdata", "nitrate", 23)        ## 1.280833
-
